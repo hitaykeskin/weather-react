f55f04df3abd7a102603f400c9cb1cfe0a068c71
hitayco <71069174+hitayco@users.noreply.github.com>
2021-03-02 13:44:43 +0300

    Integrated API for temperature,description, humidity and wind speed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Weather from "./Weather";
 import Image from "./Image";
-import Information from "./Information";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
 export default function App() {
@@ -11,7 +10,6 @@ export default function App() {
         <div className="fullCard" id="thumbnail">
           <Weather />
           <Image />
-          <Information />
           <Forecast />
           <Footer />
         </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 export default function Weather() {
+  const [city, setCity] = useState("Istanbul");
+  const [temperature, setTemperature] = useState("11");
+  const [description, setDescription] = useState("Sunny");
+  const [humidity, setHumidity] = useState("63");
+  const [wind, setWind] = useState("2");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "8db434350a59b780ed9dec3c5447cf53";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+  }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+
+  function showTemperature(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].main);
+    setHumidity(response.data.main.humidity);
+    setWind(Math.round(response.data.wind.speed));
+  }
+
   return (
     <div className="Weather">
-      <h1>Istanbul</h1>
+      <h1>{city}</h1>
       <h2>
         <div className="weather-temperature">
-          <span className="degree"> 13</span>
+          <span className="degree"> {temperature}</span>
           <span className="units">
             <a href="#" className="active" id="celsius-unit">
               °C
@@ -24,7 +49,7 @@ export default function Weather() {
           <h3 class="current-date">Today</h3>
         </div>
         <div class="col-3">
-          <form>
+          <form onSubmit={handleSubmit}>
             <label for="city">
               <input
                 type="text"
@@ -34,6 +59,7 @@ export default function Weather() {
                 id="city"
                 name="city-name"
                 autoFocus="on"
+                onChange={handleChange}
               />
               <button class="btn" title="Search">
                 🔎
@@ -45,6 +71,17 @@ export default function Weather() {
           <button class="current-location">Current Location📍</button>
         </div>
       </div>
+      <p className="description">{description}</p>
+      <p className="temp-humidity">
+        <span> Humidity:</span>
+        <span className="humidity">{humidity}</span>
+        <span>%</span>
+      </p>
+      <p className="wind-speed">
+        <span>Wind Speed: </span>
+        <span className="wind">{wind}</span>
+        <span>km/h</span>
+      </p>
     </div>
   );
 }
