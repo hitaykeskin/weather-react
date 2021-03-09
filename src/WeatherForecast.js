596867f7e28f9d466c03f991e6cc90aa0d667542
hitayco <71069174+hitayco@users.noreply.github.com>
2021-03-09 16:45:44 +0300

    Updated the API url

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,7 +24,7 @@ export default function WeatherForecast(props) {
     );
   } else {
     let apiKey = "8db434350a59b780ed9dec3c5447cf53";
-    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(handleForecastResponse);
     return null;
   }
